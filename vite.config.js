ce70534d1100489ab7534b99aa80771de8e5e70b
Mudassir <159173652+mudassir779@users.noreply.github.com>
2025-12-07 11:41:12 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss()
   ],
+  server: {
+    port: 8000,
+    host: '0.0.0.0'
+  }
 })
